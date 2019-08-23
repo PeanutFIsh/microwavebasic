1c570db182aecea1232cda10b42d0d007e8bdefd
Matthilde <peanutfish2@gmail.com>
2019-08-23 22:24:22 +0200

    Fixed compile scripts

--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
 @echo off
 title MicrowaveBASIC Epic Compiler
+
+if not exist bin\ mkdir bin
 echo Compiling MicrowaveBASIC for Windows...
 g++ *.cpp libs\*.cpp -o .\bin\microwavebasic.exe
 echo Done! Press any key to commit exit
--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
+if [ ! -d "$bin" ]; then
+	mkdir bin
+fi
+
 echo "Compiling MicrowaveBASIC Interpreter..."
 g++ *.cpp libs/*.cpp -o bin/microwavebasic
 echo "Done!"
